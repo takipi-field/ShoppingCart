e44827ca46b70d70990bb406345038d62045e9c1
Karthik Lalithraj <karthiklalithraj@Karthiks-Laptop.local>
2018-03-03 20:10:05 -0500

    Changed jar name in .sh files

--- runAllWorkflows.sh
+++ runAllWorkflows.sh
@@ -1,1 +1,1 @@
-java -Xms4G -Xmx4G -agentlib:TakipiAgent -Dtakipi.name=ShoppingCartApplication -Dtakipi.deployment.name=v1.0 -jar target/shoppingCart.jar run_mode=ALL_WORKFLOWS no_of_threads=10 no_of_iterations=10000
+java -Xms4G -Xmx4G -agentlib:TakipiAgent -Dtakipi.name=ShoppingCartApplication -Dtakipi.deployment.name=v1.0 -jar target/shopping-cart-demo-1.0.jar run_mode=ALL_WORKFLOWS no_of_threads=10 no_of_iterations=10000
