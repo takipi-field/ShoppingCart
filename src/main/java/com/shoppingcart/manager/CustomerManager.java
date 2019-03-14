b6160a52063805338ebf6883f82f64101bb186ea
Karthik Lalithraj <karthiklalithraj@Karthiks-Laptop.hsd1.ma.comcast.net>
2019-03-14 00:08:24 -0400

    More changes

--- src/main/java/com/shoppingcart/manager/CustomerManager.java
+++ src/main/java/com/shoppingcart/manager/CustomerManager.java
@@ -13,56 +13,56 @@ import com.shoppingcart.domain.Order;
 public class CustomerManager {
 
 	private CustomerDAO customerDAOImpl = new CustomerDAOImpl();
-	private final static Logger logCustomer = LoggerFactory.getLogger(CustomerManager.class);
+	private final static Logger log = LoggerFactory.getLogger(CustomerManager.class);
 			
 	public Customer get(String customerNumber) {
-		logCustomer.info("Getting a customer");
+		log.info("Getting a customer");
 		return customerDAOImpl.getByCustomerNumber(customerNumber);
 	}
 
 	public Customer create(String customerNumber) {
-		logCustomer.info("Creating a customer");
+		log.info("Creating a customer");
 		return customerDAOImpl.create(customerNumber);
 	}
 
 	public Customer create() {
-		logCustomer.info("Creating a customer");
+		log.info("Creating a customer");
 		return customerDAOImpl.create();
 	}
 
 	public Customer update(Customer customer) {
-		logCustomer.info("Updating customerNumber: " + customer.getAccountNumber());
+		log.info("Updating customerNumber: " + customer.getAccountNumber());
 		return customerDAOImpl.update(customer);
 	}
 	
 	public Order getMostRecentOrder(String customerNumber) {
-		logCustomer.info("GetMostRecent Order for a customer");
+		log.info("GetMostRecent Order for a customer");
 		return null;
 	}
 	
 	public List<Order> getAllOrders(String customerNumber) {
-		logCustomer.info("GetAllOrders for customer");
+		log.info("GetAllOrders for customer");
 		return null;
 	}
 	
 	public boolean cancelOrder(String customerNumber, String orderNumber) {
-		logCustomer.info("Cancel Order for customer");
+		log.info("Cancel Order for customer");
 		return false;
 	}
 	
 	public boolean cancelOrderDetail(String customerNumber, 
 			String orderNumber, String orderDetailNumber) {
-		logCustomer.info("Cancel Order Detail for customer");
+		log.info("Cancel Order Detail for customer");
 		return false;
 	}
 
 	public boolean delete(Customer customer) {
-		logCustomer.info("Delete for customer");
+		log.info("Delete for customer");
 		return customerDAOImpl.delete(customer);
 	}
 
 	public Customer find(String customerNumber) {
-		logCustomer.info("Find customerNumber");
+		log.info("Find customerNumber");
 		return customerDAOImpl.find(customerNumber);
 	}
 }
