9f8a14787f526e539bac6e3e4416a17cd5d2e877
Karthik Lalithraj <karthiklalithraj@Karthiks-Laptop.hsd1.ma.comcast.net>
2019-07-09 00:10:04 -0400

    Empire Retail - More Changes

--- runUncaughtExceptions.sh
+++ runUncaughtExceptions.sh
@@ -1,3 +1,3 @@
 for i in {1..10}; do
-	java -Xms4G -Xmx4G -agentlib:TakipiAgent -Dtakipi.name=ShoppingCartApplication -Dtakipi.deployment.name=v1.1 -jar target/shopping-cart-demo-1.0.jar run_mode=UNCAUGHT_EXCEPTIONS no_of_threads=10 no_of_iterations=100
+	java -Xms4G -Xmx4G -agentlib:TakipiAgent -Dtakipi.name=ShoppingCartApplication -Dtakipi.deployment.name=v1.1 -jar ../../target/shopping-cart-demo-1.0.jar run_mode=UNCAUGHT_EXCEPTIONS no_of_threads=10 no_of_iterations=100
 done
